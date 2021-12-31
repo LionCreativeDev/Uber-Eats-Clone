572d3a6a84ae1ffc8ca5f453e88e2d5b8f773250
brave_lion <brave_lion_08@yahoo.com>
2021-12-31 17:25:15 +0500

    BottomTab - initial layout created

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,10 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
-export default function BottomTabs(props) {
+const icons = [{
+    "name": "home",
+    "text": "Home"
+},{
+    "name": "search",
+    "text": "Browse"
+},{
+    "name": "shopping-bag",
+    "text": "Grocery"
+},{
+    "name": "receipt",
+    "text": "Order"
+},{
+    "name": "user",
+    "text": "Account"
+}];
+
+const Icons = (props) => (
+    <TouchableOpacity onPress={()=>{alert(`you pressed ${props.text}`)}}>
+        <View>
+            <FontAwesome5 name={props.name} size={20} style={{ marginBottom: 3, alignSelf: "center" }} />
+            <Text>{props.text}</Text>
+        </View>
+    </TouchableOpacity>
+)
+
+export default function BottomTabs() {
     return (
-        <View style={{flexDirection: "row", justifyContent: 'space-between', marginHorizontal: 10, padding: 10}}>
-            <View style={{ flexDirection: "column", alignItems: "center" }}>
+        <View style={{flexDirection: "row", justifyContent: 'space-between', padding: 10, backgroundColor: "#fff"}}>
+            {/* <View style={{ flexDirection: "column", alignItems: "center" }}>
                 <Text>Image</Text>
                 <Text>Text</Text>
             </View>
@@ -19,7 +46,18 @@ export default function BottomTabs(props) {
             <View style={{ flexDirection: "column", alignItems: "center" }}>
                 <Text>Image</Text>
                 <Text>Text</Text>
-            </View>
+            </View> */}
+
+            {icons.map((icon, index) => (
+                //console.log("icon===>", icon);
+                <Icons name={icon.name} text={icon.text} key={index} />
+            ))}
+            
+            {/* <Icons name="home" text="Home"/>
+            <Icons name="search" text="Browse"/>
+            <Icons name="shopping-bag" text="Grocery"/>
+            <Icons name="receipt" text="Order"/>
+            <Icons name="user" text="Account"/> */}
         </View>
     )
 }
